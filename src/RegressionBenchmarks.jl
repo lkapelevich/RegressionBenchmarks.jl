fae8c1a410f44b26532f9cf0fcd42d8a030fd01c
LEAXPS-15\lkape <lkapelevich@yahoo.com.au>
2018-04-04 22:22:03 -0400

    start making dists

--- src/RegressionBenchmarks.jl
+++ src/RegressionBenchmarks.jl
@@ -1,5 +1,36 @@
 module RegressionBenchmarks
 
-# package code goes here
+using StatsBase, Distributions
+import Base.rand
+
+export BinChoice,
+    getw, getX
+
+struct BinChoice <: DiscreteUnivariateDistribution end
+
+rand(::BinChoice) = sample([-1, 1])
+
+const BenchmarkXDists = Union{Normal, MvNormal, Uniform}
+const BenchmarkwDists = Union{BinChoice, Normal, Uniform}
+
+struct BenchmarkData
+    Xdist::BenchmarkXDists
+    wdist::BenchmarkwDists
+    p::Int
+end
+
+function getw(d::Int, k::Int, wdist::BenchmarkwDists)
+    w = zeros(d)
+    support = sample(1:d, k, replace=false)
+    w[support] = rand(wdist, k)
+    w
+end
+
+function getX(n::Int, d::Int, Xdist::BenchmarkXDists)
+    rand(Xdist, n, d)
+end
+function getX(n::Int, d::Int, Xdist::MvNormal)
+    rand(Xdist, n)'
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,20 @@
 using RegressionBenchmarks
-using Base.Test
+using Base.Test, Distributions
 
-# write your own tests here
-@test 1 == 2
+@testset "Types" begin
+    srand(1)
+    n = 20
+    d = 10
+    sparsity = 5
+    μ = 0.0
+    Σ = 0.1 * ones(d, d)
+    @inbounds for i = 1:d
+        Σ[i, i] = 1.0
+    end
+    Xdist = MvNormal(μ * ones(d), Σ)
+    wdist = BinChoice()
+    w = getw(d, sparsity, wdist)
+    X = getX(n, d, Xdist)
+    y = X * w
+    @test y[1] ≈ -1.72637
+end
